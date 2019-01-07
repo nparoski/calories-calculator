05df32daf4794e88813199b288c196c29672ab4d
patoske <nemanja.nemanjalol1@gmail.com>
2019-01-07 19:39:37 +0100

    Added floating numbers to 2 decimal places

--- main.js
+++ main.js
@@ -51,9 +51,9 @@ function calcTotal(){
   }
   let totalKcal = (totalCarbs*4)+(totalProts*4)+(totalFats*9);
   totalInfo.innerHTML = `
-  <h4>Calories: ${totalKcal} <span class="small">(kcal)</span></h2>
-  <h4>Carbs: ${totalCarbs} <span class="small">(g)</span></h2>
-  <h4>Proteins: ${totalProts}<span class="small">(g)</span></h2>
-  <h4>Fats: ${totalFats} <span class="small">(g)</span></h4>`
+  <h4>Calories: ${totalKcal.toFixed(2)} <span class="small">(kcal)</span></h2>
+  <h4>Carbs: ${totalCarbs.toFixed(2)} <span class="small">(g)</span></h2>
+  <h4>Proteins: ${totalProts.toFixed(2)}<span class="small">(g)</span></h2>
+  <h4>Fats: ${totalFats.toFixed(2)} <span class="small">(g)</span></h4>`
 }
 // 2. end of module|component 
